c4f56cfb854fa08534475f0b930ae7b6f6a2fbc7
Koustubh Kulkarni <130462919+koustubh-k@users.noreply.github.com>
2025-08-01 01:24:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import langchain
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_groq import ChatGroq
